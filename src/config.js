da7f3d3a9d649b157aad06cd1b01ba503f0646cc
Daniel Baah <dbaah@spatialdev.com>
2019-07-15 11:36:20 -0700

    reformat config

--- src/config.js
+++ src/config.js
@@ -8,41 +8,40 @@ const config = {
 	// Google Analytics
 	ga: {
 		// Global GA identifier
-		id: "UA-XXXXXXXX-X",
+		id: 'UA-XXXXXXXX-X',
 		// Event Category constants
 		event: {
 			// Vendor click events
 			vendor: {
-				CATEGORY: "Vendor",
+				CATEGORY: 'Vendor',
 				action: {
-					MAP_CLICK: "Map Click Select",
-					RIGHT_PANEL_VENDOR_SELECT: "Vendor List Select (Right Panel)",
-					LEFT_PANEL_VENDOR_SELECT: "Featured Vendor Select (Left Panel)"
+					MAP_CLICK: 'Map Click Select',
+					RIGHT_PANEL_VENDOR_SELECT: 'Vendor List Select (Right Panel)',
+					LEFT_PANEL_VENDOR_SELECT: 'Featured Vendor Select (Left Panel)'
 				}
 			},
 			// Geolocate "Find Me" button
 			geoLocate: {
-				CATEGORY: "GeoLocate",
+				CATEGORY: 'GeoLocate',
 				action: {
-					SELECT: "Select Geolocate button",
-					ERROR: "Error",
-					SUCCESS: "Get Position Success",
-					OUT_OF_BOUNDS: "Get Position Out of bounds"
+					SELECT: 'Select Geolocate button',
+					ERROR: 'Error',
+					SUCCESS: 'Get Position Success',
+					OUT_OF_BOUNDS: 'Get Position Out of bounds'
 				}
 			},
 			// User interaction w/ interface
 			ui: {
-				CATEGORY: "UI Interaction",
+				CATEGORY: 'UI Interaction',
 				action: {
-					RIGHT_MENU_SELECT: "Right panel open",
-					LEFT_MENU_SELECT: "Left panel open"
+					RIGHT_MENU_SELECT: 'Right panel open',
+					LEFT_MENU_SELECT: 'Left panel open'
 				}
 			}
 		}
 	},
 	map: {
-		accessToken:
-			"pk.eyJ1Ijoic3BhdGlhbGRldiIsImEiOiJjanh0bHczc2Qwdnd0M25udGQzZm9tcTBzIn0.MnBju8Y2wP6N6nFm4nNu7A",
+		accessToken: "pk.eyJ1Ijoic3BhdGlhbGRldiIsImEiOiJjanh0bHczc2Qwdnd0M25udGQzZm9tcTBzIn0.MnBju8Y2wP6N6nFm4nNu7A",
 		style: "mapbox://styles/spatialdev/cjxyccxk90prc1cpyq11zt62g",
 		center: [-122.319, 47.614],
 		bounds: [
@@ -52,8 +51,8 @@ const config = {
 			[-122.3144109050901, 47.61535256842217]
 		],
 		zoom: 17.15,
-		bearing: 270
+	  bearing: 270
 	}
-};
+}
 
-export { config };
+export {config}
