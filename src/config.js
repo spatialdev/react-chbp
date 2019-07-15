69bc9004d280c738478eeb16ba8717a119953338
Daniel Baah <dbaah@spatialdev.com>
2019-07-15 11:42:57 -0700

    reformat config part 2

--- src/config.js
+++ src/config.js
@@ -5,54 +5,40 @@
  ********************/
 
 const config = {
-	// Google Analytics
-	ga: {
-		// Global GA identifier
-		id: 'UA-XXXXXXXX-X',
-		// Event Category constants
-		event: {
-			// Vendor click events
-			vendor: {
-				CATEGORY: 'Vendor',
-				action: {
-					MAP_CLICK: 'Map Click Select',
-					RIGHT_PANEL_VENDOR_SELECT: 'Vendor List Select (Right Panel)',
-					LEFT_PANEL_VENDOR_SELECT: 'Featured Vendor Select (Left Panel)'
-				}
-			},
-			// Geolocate "Find Me" button
-			geoLocate: {
-				CATEGORY: 'GeoLocate',
-				action: {
-					SELECT: 'Select Geolocate button',
-					ERROR: 'Error',
-					SUCCESS: 'Get Position Success',
-					OUT_OF_BOUNDS: 'Get Position Out of bounds'
-				}
-			},
-			// User interaction w/ interface
-			ui: {
-				CATEGORY: 'UI Interaction',
-				action: {
-					RIGHT_MENU_SELECT: 'Right panel open',
-					LEFT_MENU_SELECT: 'Left panel open'
-				}
-			}
-		}
-	},
-	map: {
-		accessToken: "pk.eyJ1Ijoic3BhdGlhbGRldiIsImEiOiJjanh0bHczc2Qwdnd0M25udGQzZm9tcTBzIn0.MnBju8Y2wP6N6nFm4nNu7A",
-		style: "mapbox://styles/spatialdev/cjxyccxk90prc1cpyq11zt62g",
-		center: [-122.319, 47.614],
-		bounds: [
-			// Southwest
-			[-122.32140396058543, 47.612702531671545],
-			// Northeast
-			[-122.3144109050901, 47.61535256842217]
-		],
-		zoom: 17.15,
-	  bearing: 270
-	}
-}
+  "ga": {
+    "id": "UA-XXXXXXXX-X",
+    "event": {
+      "vendor": {
+        "CATEGORY": "Vendor",
+        "action": {
+          "MAP_CLICK": "Map Click Select",
+          "RIGHT_PANEL_VENDOR_SELECT": "Vendor List Select (Right Panel)",
+          "LEFT_PANEL_VENDOR_SELECT": "Featured Vendor Select (Left Panel)"
+        }
+      },
+      "geoLocate": {
+        "CATEGORY": "GeoLocate",
+        "action": {
+          "SELECT": "Select Geolocate button",
+          "ERROR": "Error",
+          "SUCCESS": "Get Position Success",
+          "OUT_OF_BOUNDS": "Get Position Out of bounds"
+        }
+      },
+      "ui": {
+        "CATEGORY": "UI Interaction",
+        "action": {"RIGHT_MENU_SELECT": "Right panel open", "LEFT_MENU_SELECT": "Left panel open"}
+      }
+    }
+  },
+  "map": {
+    "accessToken": "pk.eyJ1Ijoic3BhdGlhbGRldiIsImEiOiJjanh0bHczc2Qwdnd0M25udGQzZm9tcTBzIn0.MnBju8Y2wP6N6nFm4nNu7A",
+    "style": "mapbox://styles/spatialdev/cjxyccxk90prc1cpyq11zt62g",
+    "center": [-122.319, 47.614],
+    "bounds": [[-122.32140396058543, 47.612702531671545], [-122.3144109050901, 47.61535256842217]],
+    "zoom": 17.15,
+    "bearing": 270
+  }
+};
 
 export {config}
